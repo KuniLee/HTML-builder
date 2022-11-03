1694d4eec43470177da44dd9805572acda31c4eb
Yuriy Sitnikov <selnetys@gmail.com>
2022-11-03 16:15:29 +0500

    feat: improve 04-copy-directory by coping nested directories

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,23 +4,30 @@ const {resolve} = require('path')
 const sourceDir = resolve(__dirname, 'files')
 const distDir = sourceDir + '-copy'
 
-let fileList = []
+function copyDir(sourcePath, distPath) {
 
-mkdir(distDir, {recursive: true})
-  .then(() => readdir(sourceDir, {withFileTypes: true}))
-  .then(data => data.filter(el => el.isFile()))
-  .then(files => {
-    fileList = files
-    return files.map(el => copyFile(resolve(sourceDir, el.name),
-      resolve(distDir, el.name)))
-  })
-  .then(promises => {
-    return Promise.all(promises)
-  })
-  .then(() => readdir(distDir, {withFileTypes: true}))
-  .then(newFiles=>{
-    newFiles.forEach(newFile=>{
-      if (!fileList.find(el=>el.name === newFile.name))
-        unlink(resolve(distDir, newFile.name))
-    })
-  })
+  let fileList = []
+  return mkdir(distPath, {recursive: true})
+    .then(() => readdir(sourcePath, {withFileTypes: true}))
+    .then(data => {
+      return Promise.all(data.map(el => {
+        fileList.push(el)
+        if (el.isFile()) {
+          return copyFile(resolve(sourcePath, el.name),
+            resolve(distPath, el.name))
+        } else {
+          return copyDir(resolve(sourcePath, el.name), resolve(distPath, el.name))
+        }
+      }))
+    }).then(() => readdir(distPath, {withFileTypes: true}))
+    .then(newFiles => newFiles
+      .forEach(newFile => {
+        if (!fileList.find(el => el.name === newFile.name))
+          unlink(resolve(distPath, newFile.name))
+      })
+    )
+}
+
+copyDir(sourceDir, distDir)
+
+exports.copyDir = copyDir
