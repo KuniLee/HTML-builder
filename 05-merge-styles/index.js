0009ec6bafa194094a47892d7e955b0101406771
Yuriy Sitnikov <selnetys@gmail.com>
2022-11-03 18:11:12 +0500

    feat: improve 05-merge-styles by export function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,27 +5,33 @@ const {readdir} = require('fs/promises')
 const pathToStyles = resolve(__dirname, 'styles')
 const pathToBundle = resolve(__dirname, 'project-dist', 'bundle.css')
 
-readdir(pathToStyles, {withFileTypes: true}).then(data => data.filter(el =>
-  el.isFile() && extname(resolve(pathToStyles, el.name)) === '.css'))
-  .then(res => Promise.all(res.map(file => new Promise(resol => {
-    let data = ''
-    const readStream = createReadStream(resolve(pathToStyles, file.name))
-    readStream.on('data', chunk => {
-      data += chunk
+function mergeStyles(stylesPath, bundlePath) {
+  return readdir(stylesPath, {withFileTypes: true}).then(data => data.filter(el =>
+    el.isFile() && extname(resolve(stylesPath, el.name)) === '.css'))
+    .then(res => Promise.all(res.map(file => new Promise(resol => {
+      let data = ''
+      const readStream = createReadStream(resolve(stylesPath, file.name))
+      readStream.on('data', chunk => {
+        data += chunk
+      })
+      readStream.on('end', () => {
+        resol(data)
+      })
+    }))))
+    .then(data => {
+      const bundle = createWriteStream(bundlePath)
+      bundle.on('error', err => {
+        console.error(err)
+      })
+      data.forEach((el) => {
+        bundle.write(el)
+      })
     })
-    readStream.on('end', () => {
-      resol(data)
+    .then(() => {
+      console.log('bundle created')
     })
-  }))))
-  .then(data => {
-    const bundle = createWriteStream(pathToBundle)
-    bundle.on('error', err => {
-      console.error(err)
-    })
-    data.forEach((el) => {
-      bundle.write(el)
-    })
-  })
-  .then(() => {
-    console.log('bundle created')
-  })
+}
+
+mergeStyles(pathToStyles, pathToBundle)
+
+exports.mergeStyles = mergeStyles
