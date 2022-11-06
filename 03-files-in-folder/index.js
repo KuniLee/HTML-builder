56cc78dc6e03c72488c90987bbbff5b4c12c09c7
Sitnikov <selnetys@gmail.com>
2022-11-06 18:45:30 +0500

    refactor: rewrite 03 task to async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,20 @@
 const {readdir, stat} = require('fs/promises')
 const {resolve, extname, basename} = require('path')
 
-
 const pathToDir = resolve(__dirname, 'secret-folder')
 
+async function dirInfo() {
+  const info = await readdir(pathToDir, {withFileTypes: true})
+  const filePaths = info.filter(el => el.isFile()).map(el => resolve(pathToDir, el.name))
+  const stats = await Promise.all(filePaths.map(filePath => stat(filePath)))
+  let result = ''
+  filePaths.forEach((file, idx) => {
+    const fileName = basename(file, extname(file))
+    const ext = extname(file).slice(1)
+    const size = stats[idx].size / 1000 + 'kb'
+    result += ([fileName, ext, size].filter(el => el).join(' - ')) + '\r\n'
+  })
+  return result.trim()
+}
 
-readdir(pathToDir, {withFileTypes: true}).then(res => {
-
-  // return array of files only paths
-  return res.filter(el => el.isFile()).map(el => resolve(pathToDir, el.name))
-}).then(paths => {
-  Promise.all(paths.map(filePath => stat(filePath))) // get array of promises of stats
-    .then((stats) => {
-      paths.forEach((file, idx) => {
-        const fileName = basename(file, extname(file))
-        const ext = extname(file).slice(1)
-        const size = stats[idx].size / 1000 + 'kb'
-
-        // generate file info string
-        console.log([fileName, ext, size].filter(el => el).join(' - '))
-      })
-    })
-})
+dirInfo().then(console.log)
